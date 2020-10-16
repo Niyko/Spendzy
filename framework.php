3c34e20d2d600e96c79a18e520ac3326e9d744b8
Niyko <niyko.official@gmail.com>
2020-10-16 15:18:48 +0530

    Fixed script cache

--- framework.php
+++ framework.php
@@ -2,4 +2,8 @@
     function add_css($file){
         echo "<link rel='stylesheet' href='${file}?i=".filemtime($file)."'>";
     }
+
+    function add_script($file){
+        echo "<script src='${file}?i=".filemtime($file)."'></script>";
+    }
 ?>
--- widgets/scripts.php
+++ widgets/scripts.php
@@ -1,4 +1,4 @@
-<script src="js/config.js"></script>
+<?php add_script("js/config.js"); ?>
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.8/dist/js/uikit.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
@@ -6,5 +6,5 @@
 <script src="https://www.gstatic.com/firebasejs/7.22.1/firebase-firestore.js"></script>
 <script src='https://cdn.jsdelivr.net/gh/Niyko/href-For-EveryThing/src/hrefForEveryThing.js'></script>
 <script type="text/javascript" src="js/ripple.js"></script>
-<script src="js/database.js"></script>
-<script src="js/main.js"></script>
+<?php add_script("js/database.js"); ?>
+<?php add_script("js/main.js"); ?>
